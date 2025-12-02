dc62694af0ef6bdc4839f71ff61fa01189c0de05
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-12-02 23:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 class EventTarget {
     constructor() {
-      this.listeners = new Map();
+      this.listeners = {};
     }
   
     addEventListener(event, callback) {
-      
+      if(!this.listeners[event]){
+		  this.listeners[event]=new Set();
+	  }
+		this.listeners[event].add(callback);
     }
   
     removeEventListener(event, callback) {
-      
+      if(!this.listeners[event]) return;
+	   this.listeners[event].delete(callback);
     }
   
     dispatchEvent(event) {
-    
+	    if(!this.listeners[event]) return;
+		for(let callback of this.listeners[event]){
+			callback();
+		}
     }
     
   }
